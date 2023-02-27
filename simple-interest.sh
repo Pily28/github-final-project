02c2e072ac643ce8a9e0368616b17cce7ccd9510
Priscilla Moncrieffe <87598302+Pily28@users.noreply.github.com>
2023-02-26 18:29:31 -0600

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Priscilla Moncrieffe
    # Additional Authors:
-   # <your GitHub username>
+   # <your GitHub username> Pily28
 
    # Input:
    # p, principal amount
